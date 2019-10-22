89d6a2e61290893c78628c183181fd5aecbc5ce0
Aqil <aqilabdaziz1123@gmail.com>
2019-10-22 12:13:57 +0800

    mesininput diubah dikit soalnya kalo make ADT awal gw ga ngerti cara assignnya

--- bangunan.h
+++ bangunan.h
@@ -6,6 +6,8 @@
 
 #include "boolean.h"
 #include "point.h"
+#include "queue.h"
+#include "matriks.h"
 
 typedef struct {
 	int A; 
@@ -24,13 +26,26 @@ typedef struct {
 }JNS;
 
 
+
 typedef struct {
 	char Milik; //Terdiri dari A untuk player 1, B untuk player 2, X untuk bangunan yang belum dikuasai
     JNS Jenis;  //Jenis Bangunannya
     int Jpas;   //Jumlah pasukan
     int Level;  //Level
+    char jenis; //for now biar gw bisa progress mesininput gini, gw ga paham cara ngeassign jenis kalo bentuknya kaya diatas
+    int A[5],M[5],U;
+    boolean P[5];
     POINT Lok;  //Menunjukkan Lokasi. Diakses dengan Lok.X dan Lok.Y
+    int hubungan[31];
 }BANGUNAN;
+
+typedef struct {
+    BANGUNAN listb[31];
+    int JBang;
+    int turn;
+    Queue sA, sB;
+    MATRIKS peta;
+}STATE;
 
 
 #endif
