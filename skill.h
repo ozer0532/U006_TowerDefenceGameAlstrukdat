9940926ab6e1abbcdfbde1eebdba67c0cedd4682
Fatkhan Masruri <fatkhan.masruri45@gmail.com>
2019-10-26 23:59:42 +0700

    skill beberapa masih salah

--- skill.h
+++ skill.h
@@ -1,17 +1,17 @@
 #ifndef SKILL_H
 #define SKILL_H
 #include "bangunan.h"
-#include "graph.h"
-#include "queue.h"
+#include "listlinier.h"
+#include "array.h"
 
 
-void InstantUpgrade ();
+void InstantUpgrade (Pemain Pe, BangunanTot *Ba, int PemainKe);
 
     /* Pemain mendapatkan skill ini pada skill awal
         I.S Penain mungkin belom punya bangunan
         F.S Setiap banguna yang dimiliki pemain naik satu level*/
 
-void Shield (); //Bonus
+void Shield (Pemain Pe, BangunanTot *Ba, int PemainKe); //Bonus
     /*Seluruh bangunan yang dimiliki pemain akan memiliki pertahanan selama 2 turn
         apabila pemain menggunakan skill ini 2 kali beturut-turut durasi tidak akan bertamabah, namun menjadi nilai maksimum    */
 
@@ -24,10 +24,10 @@ void AttackUp(); //Bonus
 void CriticalHit (); //Bonus
     /* Syarat: Musuh baru saja melakukan skill Extra Turn
         F.S: pada bangunan yang melakukan serangan tepta selanjutnya hanya berkurang 1/2 dari jumlah seharusnya. */
-void InstantReinforcement();
+void InstantReinforcement(Pemain Pe, BangunanTot *Ba, int PemainKe);
     /* Syarat : Diakhir gilirannya, bila semua bangunan yang ia miliki memiliki level 4
         F.S: Seluruh bangunan yang ia miliki bertambah 5 pasukannya*/
-void Barrage ();
+void Barrage (Pemain Pe, BangunanTot *Ba, int PemainKe);
     /* Syarat: Jumalah bangunan lawan baru saja menjadi 10
         F.S: Jumlah pasukan pada setiap bangunan lawan berkurang 10 */
 
