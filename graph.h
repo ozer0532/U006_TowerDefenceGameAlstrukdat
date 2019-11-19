7f3efc0cd18ad405da44d53a945284f3e3076f74
ozer0532itb <ozer0532@students.itb.ac.id>
2019-11-20 00:00:20 +0700

    Graph versi terbaru :) ~zer

--- graph.h
+++ graph.h
@@ -4,37 +4,62 @@
 #define GRAPH_H
 
 #include "boolean.h"
-#include "graph.h"
 #include "point.h"
 #include "array.h"
 
-/* Ukuran minimum dan maksimum baris dan kolom */
-#define IdxMin 1
-#define IdxMaxG 30
+#define vertex int
+#define edge POINT
+
+typedef struct tElmtRow * addrRow;
+typedef struct tElmtCol * addrCol;
+
+typedef struct tElmtRow {
+    vertex info;
+    addrCol branch;
+    addrRow next;
+} elmtRow;
+
+typedef struct tElmtCol{
+    vertex info;
+    addrCol next;
+} elmtCol;
 
 typedef struct {
-    boolean Mem[IdxMaxG][IdxMaxG];
-    int Neff; /* banyaknya/ukuran baris yg terdefinisi */
+    addrRow First;
 } Graph;
-/* Jika x = Baris dan y = Kolom, maka Mem[x][y] = 1 menunjukkan bahwa terdapat relasi x->y */
 
-typedef struct {
-    boolean TI[IdxMaxG+1];
-} ArrayVertex;
+#define Branch(P) (P)->branch
 
 /* *** Selektor *** */
 #define Neff(M) (M).Neff
 #define ElmtM(M,i,j) (M).Mem[(i)][(j)]
 
-#define vertex int
-#define edge POINT
-
-Graph CreateEmptyGraph (vertex V[], edge E[]);
+Graph CreateEmptyGraph ();
 /* Neff = V.length */
 /* Graph berisi matriks adjacency hubungan antar V menurut E */
 /* Memori yang dipakai mulai dari indeks 1,1 */
 
-boolean IsEmptyGR (Graph G);
+addrRow AlokRow (vertex X);
+
+addrCol AlokCol (vertex X);
+
+int NbElmtRow (Graph G);
+/* Mengirimkan banyaknya row graph; mengirimkan 0 jika list kosong */
+
+int NbElmtCol (addrRow R);
+/* Mengirimkan banyaknya column row; mengirimkan 0 jika list kosong */
+
+void AddElmtG (Graph * G, vertex X);
+/* I.S. G terdefinisi */
+/* F.s. X ditambahkan di akhir list graph G */
+
+void AddElmtR (addrRow * R, vertex X);
+/* I.S. R terdefinisi */
+/* F.s. X ditambahkan di akhir list row R */
+
+void AddRelation (Graph * G, vertex from, vertex to);
+
+boolean IsEmptyG (Graph G);
 /* Neff = 0 */
 
 boolean Adjacent (Graph G, vertex v1, vertex v2);
@@ -44,7 +69,7 @@ boolean Adjacent (Graph G, vertex v1, vertex v2);
 boolean Incident (Graph G, vertex v, edge e);
 /* Mengetes bila v dan e berhubungan */
 
-ArrayVertex Neighbors (Graph G, vertex v);
+addrCol Neighbors (Graph G, vertex v);
 /* Mengoutput semua vertex yang merupakan relasi dari v */
 
 /* Gak aku buat karena gak bakal dipake juga...
