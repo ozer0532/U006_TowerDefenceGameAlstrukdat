f81830489c2801f267c50e31d3e1c84287dbe43a
ozer0532itb <ozer0532@students.itb.ac.id>
2019-11-21 21:26:00 +0700

    Added compile command ~zer

---
+++ buildMain.cmd
@@ -0,0 +1,7 @@
+@echo off
+cls
+echo Building Program...
+
+gcc -o main array.c attack.c bangunan.c graph.c listlinier.c main.c matriks.c mesininput.c mesinkar.c move.c pcolor.c point.c queue.c skill.c stackt.c STATE.c
+
+pause
